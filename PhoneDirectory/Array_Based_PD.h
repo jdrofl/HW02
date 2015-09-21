522b1555b9dab2fa33f823183b0838ae01c17c31
jdrofl <jgeldbach18@my.whitworth.edu>
2015-09-21 09:56:02 -0700

    Corrected the code within the Directory_Entry class public

--- PhoneDirectory/Array_Based_PD.h
+++ PhoneDirectory/Array_Based_PD.h
@@ -56,6 +56,7 @@ private:
 	class Directory_Entry /* Exercise 1.6: Please complete the definition of the Directory_Entry class here. - Ed/Kent */
 	{
 	public:
+
 		Directory_Entry() {} // Default no-argument constructor
 		Directory_Entry(std::string the_name, std::string the_number) {
 			// constructor not implemented yet
@@ -69,7 +70,6 @@ private:
 		void set_number(const std::string& new_number) {
 			// method not implemented yet
 		}
-
 	private:
 
 	};
